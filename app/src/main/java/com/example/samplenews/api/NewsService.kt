8f6ce3d0e5e5e7615b8cdf8a406744e1b5d0e374
Sachin Pechhetti <sachin2005p@gmail.com>
2019-06-27 04:35:49 +0530

    Reverted unnecessary changes

--- app/src/main/java/com/example/samplenews/api/NewsService.kt
+++ app/src/main/java/com/example/samplenews/api/NewsService.kt
@@ -11,7 +11,7 @@ import retrofit2.http.Path
 interface NewsService {
 
     // Replace "yourkey" with original key
-    @GET("svc/mostpopular/v2/viewed/{days}.json?api-key=HQ35yIYJXbUvUTIB0z41w8LKG80AvmIM")
+    @GET("svc/mostpopular/v2/viewed/{days}.json?api-key=yourkey")
     fun getMostPopularNews(@Path("days") days: Int): Observable<NewsFeedsModel>
 
 }
